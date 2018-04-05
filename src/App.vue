fa21d1d676729bcc3991dd8d97cae307cf3ca026
Ruston Emperua <rustonemperua@elink.com.ph>
2018-04-05 13:40:23 +0800

    added fixed footer

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
     <div class="container">
       <router-view/>
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer class="footer">
+        <div class="container text-center">
+            <span class="text-muted">Copyright &copy; {{new Date().getFullYear()}} eLink Systems &amp; Concepts Corp.</span>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+    .footer {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 60px;
+        line-height: 60px;
+        background-color: #343a40;
+    }
+</style>
